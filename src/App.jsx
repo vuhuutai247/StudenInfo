9af447ba7c896a49c9b82b144bfa80a804257f18
vuhuutai247 <vuhuutai1@gmail.com>
2023-07-07 10:22:56 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Student from './Components/StudentInfoComponent/StudentInfo';
+
+function App() {
+  return (
+    <>
+     <Student/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/StudentInfoComponent/StudentInfo.jsx
@@ -0,0 +1,36 @@
+import "./Studentinfo.css";
+
+function Student() {
+    return(
+        <>
+         <table>
+            <>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Address</th>
+                </tr>
+            </thead>
+            </>
+            <>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Nguyen Van A</td>
+                        <td>30</td>
+                        <td>Ha Noi</td>
+                    </tr>
+                </tbody>
+            </>
+         </table>
+        </>
+    )
+
+}
+
+
+    
+
+export default Student;
